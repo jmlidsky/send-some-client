70a13e7a34dfb48af6f9a3450430e45ff662c786
Jill Lidsky <jilllidsky15@gmail.com>
2020-06-30 18:16:02 -0400

    added validation to signup page

--- src/components/SignupPage/SignupPage.js
+++ src/components/SignupPage/SignupPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import config from '../../config'
+import ValidationError from '../../components/ValidationError/ValidationError'
 import './SignupPage.css'
 
 class SignupPage extends Component {
@@ -9,9 +10,18 @@ class SignupPage extends Component {
         this.state = {
             error: null,
             email: '',
-            username: '',
-            password: '',
-            confirmPassword: '',
+            username: {
+                touched: false,
+                value: ''
+            },
+            password: {
+                touched: false,
+                value: ''
+            },
+            confirmPassword: {
+                touched: false,
+                value: ''
+            },
         }
     }
 
@@ -23,27 +33,69 @@ class SignupPage extends Component {
 
     handleChangeUsername = (e) => {
         this.setState({
-            username: e.target.value
+            username: {
+                touched: true,
+                value: e.target.value
+            }
+
         })
     }
 
     handleChangePassword = (e) => {
         this.setState({
-            password: e.target.value
+            password: {
+                touched: true,
+                value: e.target.value
+            }
         })
     }
 
     handleChangeConfirmPassword = (e) => {
         this.setState({
-            confirmPassword: e.target.value
+            confirmPassword: {
+                touched: true,
+                value: e.target.value
+            }
         })
+    }
+
+    validateUsername() {
+        const username = this.state.username.value.trim()
+        if (username.length === 0) {
+            return 'Username is required'
+        }
+        else if (username.length < 3) {
+            return 'Username must be at least 3 characters long'
+        }
+    }
+
+    validatePassword() {
+        const password = this.state.password.value.trim()
+        if (password.length === 0) {
+            return 'Password is required';
+        } else if (password.length < 6 || password.length > 20) {
+            return 'Password must be between 6 and 20 characters long'
+        }
+    }
+
+    validateConfirmPassword() {
+        const confirmPassword = this.state.confirmPassword.value.trim()
+        const password = this.state.password.value.trim()
+
+        if (confirmPassword !== password) {
+            return 'Passwords do not match'
+        }
     }
 
     handleSignupSubmit = (e) => {
         e.preventDefault()
+        // console.log(this.state.username.value)
 
-        const { email, username, password } = this.state
-        const newUser = { email, username, password }
+        const { email } = this.state
+        const username = this.state.username.value
+        const password = this.state.password.value
+        const newUser = { email, username: username, password: password }
+        // console.log(JSON.stringify(newUser))
 
         fetch(`${config.API_ENDPOINT}/auth/signup`, {
             method: 'POST',
@@ -66,26 +118,39 @@ class SignupPage extends Component {
     }
 
     render() {
+        const usernameError = this.validateUsername()
+        const passwordError = this.validatePassword()
+        const confirmPasswordError = this.validateConfirmPassword()
+        
         return (
             <form className="signup-form" onSubmit={this.handleSignupSubmit}>
                 <h3>Sign Up</h3>
                 <div>
                     <label htmlFor="signup-form-email">Email</label>
-                    <input required name="email" className="signup-form-email" onChange={this.handleChangeEmail}></input>
+                    <input required type="email" name="email" className="signup-form-email" onChange={this.handleChangeEmail}></input>
                 </div>
                 <div>
                     <label htmlFor="signup-form-username">Username</label>
-                    <input required name="username" className="signup-form-username" onChange={this.handleChangeUsername}></input>
+                    <input required type="text" name="username" className="signup-form-username" onChange={this.handleChangeUsername}></input>
+                    {this.state.username.touched && (<ValidationError message={usernameError} />)}
                 </div>
                 <div>
                     <label htmlFor="signup-form-password">Password</label>
                     <input required type="password" name="password" className="signup-form-password" onChange={this.handleChangePassword}></input>
+                    {this.state.password.touched && (<ValidationError message={passwordError} />)}
                 </div>
                 <div>
                     <label htmlFor="signup-form-confirm-password">Confirm Password</label>
                     <input required type="password" name="confirm-password" className="signup-form-confirm-password" onChange={this.handleChangeConfirmPassword}></input>
+                    {this.state.confirmPassword.touched && (<ValidationError message={confirmPasswordError} />)}
                 </div>
-                <button className="signup-button">Sign Up</button>
+                <button className="signup-button" type="submit"
+                    disabled={
+                        this.validateUsername() ||
+                        this.validatePassword() ||
+                        this.validateConfirmPassword()
+                    }
+                >Sign Up</button>
             </form>
         )
     }
